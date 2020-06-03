fd8f964aa43151032cbaf5b03604e0740a2efca1
zainrax <patrickb321@gmail.com>
2020-06-03 22:16:32 +1200

    feat(coredata): add persistant store container

---
+++ UserList/CoreDataContainer.swift
@@ -0,0 +1,32 @@
+//
+//  CoreDataContainer.swift
+//  UserList
+//
+//  Created by abra on 3/06/20.
+//  Copyright © 2020 LogosEros. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataContainer {
+    private let model: String
+    
+    init(model: String) {
+        self.model = model
+    }
+    
+    private lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.model)
+        container.loadPersistentStores{ (description, error) in
+            if let error = error as NSError? {
+                print("Unresolved Error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        return self.storeContainer.viewContext
+    }()
+}
